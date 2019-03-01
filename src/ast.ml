ab81018e84d19876a2985e52961a708d4925d631
Amanda Liu <al3623@columbia.edu>
2019-03-01 16:32:23 -0500

    Start AST

---
+++ src/ast.ml
@@ -0,0 +1,6 @@
+type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq 
+    | And | Or | Mod | Cons | Cat | Len | Head | Tail
+
+type baseType = Int | Bool | Float | Char
+
+type typeVar = Tvar of string
