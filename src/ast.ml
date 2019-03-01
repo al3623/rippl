b7133ff45fa630d32ea1a1f019826481221da259
Amanda Liu <al3623@columbia.edu>
2019-03-01 16:38:45 -0500

    Add type heirarchy

--- src/ast.ml
+++ src/ast.ml
@@ -1,6 +1,9 @@
 type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq 
     | And | Or | Mod | Cons | Cat | Len | Head | Tail
 
-type baseType = Int | Bool | Float | Char
-
-type typeVar = Tvar of string
+type ty = Int | Bool | Float | Char
+    | Tvar of string
+    | Arrow of (ty * ty)
+    | TconList of ty
+    | TconTuple of ty
+    | Tforall of ty
