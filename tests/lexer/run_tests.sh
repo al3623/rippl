589e9af89e4419bbed61f2a7cc1f745a05b546c1
Amanda Liu <al3623@columbia.edu>
2019-03-13 13:45:38 -0400

    Add diff to lexing tests

--- tests/lexer/run_tests.sh
+++ tests/lexer/run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#assert.sh
+
 make
 cd lexing_tests
 
@@ -6,7 +8,13 @@ num_tests=$(ls -1q test* | wc -l)
 
 for var in `seq 1 1 $num_tests`
 do
-    cat test$var | ../lexdriver.native | tee "$var.out"
+    cat test$var | ../lexdriver.native > "$var.out"
+	file_diff=$(diff $var.out ../lexing_output)
+	rm "$var.out"
+	if [ -z "$file_diff"] 
+	then echo "passed $var"
+   	else  exit 1 
+	fi
 done
 
 cd -
