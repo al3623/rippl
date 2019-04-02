d024d6c9a039b87be0558dab73c5918faee7f1eb
Amanda Liu <al3623@columbia.edu>
2019-04-02 18:14:25 -0400

    Add Tast file and make hello world work

--- src/tast.mli
+++ src/tast.mli
@@ -6,9 +6,11 @@ type typed_decl = TypedVdef of (string * typed_expr)
 
 type typed_program = typed_decl list
 *)
-type typed_expr =
+type typed_expr = tx * ty
+and
+tx =
     | TIntLit of int | TFloatLit of float | TBoolLit of bool 
-    | TCharLit of char
+    | TCharLit of char | TWildCard
     | TAdd | TSub | TMult | TDiv | TMod | TPow
     | TAddF | TSubF | TMultF | TDivF | TPowF | TNeg
     | TEq | TEqF | TNeq | TNeqF | TGeq | TGeqF | TLeq | TLeqF
@@ -16,18 +18,18 @@ type typed_expr =
     | TAnd | TOr | TNot 
     | TCons | TCat | TLen | THead | TTail
     | TVar of string
-    | TLet of (tassign * typed_expr * ty)
-    | TLambda of (typed_expr * typed_expr * ty)
-    | TApp of (typed_expr * typed_expr * ty)
-    | TIte of (typed_expr * typed_expr * typed_expr * ty)
-	| TListComp of (typed_expr * tclause list * ty)
-	| TListRange of (typed_expr * typed_expr * ty)
-	| TInfList of (typed_expr * ty)
-    | TListLit of (typed_expr list * ty)
+    | TLet of (tassign * typed_expr)
+    | TLambda of (typed_expr * typed_expr)
+    | TApp of (typed_expr * typed_expr)
+    | TIte of (typed_expr * typed_expr * typed_expr)
+	| TListComp of (typed_expr * tclause list)
+	| TListRange of (typed_expr * typed_expr)
+	| TInfList of typed_expr
+    | TListLit of typed_expr list
 and tclause = 
 	| TListVBind of (typed_expr * typed_expr)
 	| TFilter of typed_expr
-and tassign = TAssign of (typed_expr * typed_expr * ty)
+and tassign = TAssign of (typed_expr * typed_expr)
 
 type typed_decl =
     | TypedVdef of (string * typed_expr)
