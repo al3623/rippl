8b9ccc03fed5f7bfa7d61b884854adfe138f264c
Hans Montero <hjm2133@columbia.edu>
2019-04-05 16:00:47 -0400

    Add get_fresh_var to utils

--- src/passes/pair_annots.ml
+++ src/passes/pair_annots.ml
@@ -1,8 +1,8 @@
 open Ast
 open Scanner
 open Parser
+open Get_fresh_var
 
-let counter = ref 0
 let rec pair_helper decs last_annot annot = 
 	if (List.length decs) = 0 then 
 		if not last_annot then [] else raise (Failure "unmatched type annotation")
@@ -16,8 +16,7 @@ let rec pair_helper decs last_annot annot =
 				(Annot(fst annot, snd annot), Vdef(vname, vexpr)) :: pair_helper (List.tl decs) false annot
 			else raise (Failure "mismatched identifier name in annotation and declaration")
 		| false -> 
-			let vapair = (Annot(vname, Tvar("t" ^ string_of_int !counter)), Vdef(vname, vexpr)) in
-			counter := !counter + 1;
+			let vapair = (Annot(vname, Tvar("t" ^ get_fresh)), Vdef(vname, vexpr)) in
 			vapair :: pair_helper (List.tl decs) false annot)
 let pair_av prog = match prog with
 	| x :: xs -> pair_helper (x :: xs) false ("", Tvar(""))
--- src/utils/pretty_type_print.ml
+++ src/utils/pretty_type_print.ml
@@ -1,4 +1,84 @@
- open Ast
+open Ast
+
+let op_to_str s =
+    match s with
+    (* Boolean Operators *)
+    | Or -> "or"
+    | And -> "and"
+    | Not -> "not"
+    | Eq -> "=="
+    | EqF -> "==."
+    | Neq -> "!="
+    | NeqF -> "!=."
+    | Less -> "<"
+    | LessF -> "<."
+    | Greater -> ">"
+    | GreaterF -> ">."
+    | Leq -> "<="
+    | LeqF -> "<=."
+    | Geq -> ">="
+    | GeqF -> ">=."
+    (* Math Operations *)
+    | Add -> "+"
+    | Sub -> "-"
+    | Mult -> "*"
+    | Div -> "/"
+    | AddF -> "+."
+    | SubF -> "-."
+    | MultF -> "*."
+    | DivF -> "/."
+    | Pow -> "^"
+    | PowF -> "^."
+    | Neg -> "neg"
+    (* List Operations *)
+    | Cons -> "cons"
+    | Cat -> "cat"
+    | Head -> "head"
+    | Tail -> "tail"
+    | Len -> "len"
+    | _ -> "other"
+
+let char_list_to_str cl =
+    match cl with
+    | [] -> ""
+    | [Ast.CharLit c] -> String.make 1 c
+    | charlist -> let convert x = match x with 
+        | Ast.CharLit c -> c 
+        | _ -> 'F'
+        in List.fold_left (^) "" (List.map (String.make 1) (List.map convert charlist))
+
+let rec ast_to_str exp =
+    match exp with
+    | Ite(e1,e2,e3) -> "Ite(" ^ (ast_to_str e1) ^ "," ^ (ast_to_str e2) ^ "," ^ (ast_to_str e3) ^ ")"
+    | Let(e1,e2) -> "Let(" ^ (assign_to_str e1) ^ "," ^ (ast_to_str e2) ^ ")"
+    | Lambda(e1,e2) -> "Lambda(" ^ (ast_to_str e1) ^ "," ^ (ast_to_str e2) ^ ")"
+    | App( App(op, arg1), arg2) -> "App(App(" ^ op_to_str op ^ "," ^ (ast_to_str arg1) ^ ")," ^ (ast_to_str arg2) ^ ")"
+    | App(op, e) -> "App(" ^ (op_to_str op) ^ "," ^ (ast_to_str e) ^ ")"
+    | Var(s) -> "Var(" ^ s ^ ")"
+
+    (* Lists *)
+    | ListLit(char_list) -> "\"" ^ (char_list_to_str char_list) ^ "\""
+    | ListRange(e1, e2) -> "ListRange(" ^ (ast_to_str e1) ^ "," ^ (ast_to_str e2) ^ ")"
+    | InfList(e) -> "InfList" ^ (ast_to_str e)
+    | ListComp(e, c) -> "ListComp(" ^ (ast_to_str e) ^ "," ^ (clauses_to_str c)
+
+    | BoolLit(b) -> string_of_bool b
+    | CharLit(c) -> String.make 1 c
+    | IntLit n -> string_of_int n
+    | FloatLit f -> string_of_float f
+    
+    | WildCard -> "WildCard"
+    | _ -> ""
+
+and assign_to_str = function | Assign (e1,e2)
+    -> (e1) ^ "=" ^ (ast_to_str e2)
+
+and clauses_to_str clauses =
+   match clauses with
+   | [Filter(e)] -> "Filter(" ^ (ast_to_str e)
+   | [ListVBind(e, l)] -> "ListVBind(" ^ (ast_to_str e) ^ "," ^ (ast_to_str l) ^ ")"
+   | h::t -> (clauses_to_str [h]) ^ clauses_to_str t
+   | [] -> ""
 
  let rec ty_to_str ty =
     match ty with
