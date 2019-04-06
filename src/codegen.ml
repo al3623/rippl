bf80b1a8b7ef717f6670c10d90dfa1e3961c43a4
Amanda Liu <al3623@columbia.edu>
2019-04-05 23:24:07 -0400

    Add void * equivalent and struct generating comments

--- src/codegen.ml
+++ src/codegen.ml
@@ -8,25 +8,59 @@ let translate decl_lst =
 	(* int, char, bool, float, void *)
 	let i32_t      	 = L.i32_type    context
 	  and i8_t       = L.i8_type     context
-	  (*and i1_t       = L.i1_type     context
-	  and float_t    = L.double_type context 
-	  and void_t     = L.void_type   context*) in
+	  (*and float_t    = L.double_type context 
+	  and void_t     = L.i8_type   context*) in
 
 	(*let ltype_of_typ = function
       Int   -> i32_t
-    | Bool  -> i1_t
+    | Bool  -> i8_t
     | Float -> float_t
     | Char -> i8_t
   	in*)
+	
+	(* 
+		struct Node {
+			void *data;
+			struct Node *next;		
+		};
+	*)
+	let struct_node_type : L.lltype = L.named_struct_type context "Node" in
+	L.struct_set_body struct_node_type 
+		[| L.pointer_type i8_t ; L.pointer_type struct_node_type |] false; 
+ 
+	(* 
+		struct Tuple {
+			int t1;
+			int t2;
+			void *first;
+			void *second;
+		}; 
+	*) 
+	let struct_tuple_type : L.lltype = L.named_struct_type context "Tuple" in
+	L.struct_set_body struct_tuple_type 
+		[| i32_t ; i32_t ; L.pointer_type i8_t ; L.pointer_type i8_t |] false;	
 
-  	
+	(*
+		struct Maybe {
+			int ty;
+			int is_none;
+			void *data;
+		}
+	*)
+	let struct_maybe_type : L.lltype = L.named_struct_type context "Maybe" in
+	L.struct_set_body struct_maybe_type
+		[| i32_t ; i32_t ; L.pointer_type i8_t |] false;
+	
+	(* declare void printf(char *,...) *)	
+	let printf_t : L.lltype = 
+	      L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
+	let printf_func : L.llvalue = 
+	      L.declare_function "printf" printf_t the_module in
+
+	(* find and execute/print main *)
   	let print_main vd = match vd with
   		| (_, (TypedVdef ("main", (TListLit(clist),ty) ))) -> 
-  			let printf_t : L.lltype = 
-		      L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
-			let printf_func : L.llvalue = 
-		      L.declare_function "printf" printf_t the_module in
-		    let main_t = L.function_type i32_t [| |] in
+  					    let main_t = L.function_type i32_t [| |] in
 		    let main_f = L.define_function "main" main_t the_module in
 		    let builder = L.builder_at_end context (L.entry_block main_f) in
 		    let char_format_str = L.build_global_stringptr "%c" "fmt" builder in
