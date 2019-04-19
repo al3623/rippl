6c78a29fbfd8c399b68db8b992318b3a405f3dbc
Amanda Liu <al3623@columbia.edu>
2019-04-19 14:39:40 -0400

    Get rid of compiler warning

--- src/compiler.ml
+++ src/compiler.ml
@@ -70,10 +70,10 @@ let _ =
     let program = Parser.program Scanner.token lexbuf in
     let m_program = Lift_lambdas.transform_main program in
     let program_ll = List.fold_left lift_decl [] m_program in
-	  let pair_program = Pair_annots.pair_av program_ll in
+	let pair_program = Pair_annots.pair_av program_ll in
     let pair_iprogram = Type_inference.type_paired_program pair_program in
-		let pair_tprogram = Remove_substs.remove_subst_pairs pair_iprogram in
-		    print_all_types pair_tprogram;  
+	let pair_tprogram = Remove_substs.remove_subst_pairs pair_iprogram in
+	let _ = print_all_types pair_tprogram in
     let m = Codegen.translate pair_tprogram in
 	    Llvm_analysis.assert_valid_module m;
     let ls = Llvm.string_of_llmodule m in
