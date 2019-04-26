403ba3621051c8cbbb52b875a3af95fb48cd0e45
Hans Montero <hjm2133@columbia.edu>
2019-04-26 11:48:16 -0400

    Simplify compiler

--- src/compiler.ml
+++ src/compiler.ml
@@ -62,7 +62,7 @@ let _ =
     let m_program = Lift_lambdas.transform_main program in
     let program_ll = Lift_lambdas.close_and_lift m_program in
     List.iter print_decls program_ll;
-	let pair_program = Pair_annots.pair_av program_ll in
+	(*let pair_program = Pair_annots.pair_av program_ll in
     let pair_iprogram = Type_inference.type_paired_program pair_program in
 	let pair_tprogram = Remove_substs.remove_subst_pairs pair_iprogram in
 	let _ = print_all_types pair_tprogram in
@@ -78,4 +78,4 @@ let _ =
 		else if (command 
 			("gcc -o " ^ base_no_path ^" " ^ file ^ ".s lib.o thunk.o") != 0)
 			then raise (Failure "gcc: non-zero exit code")
-		else ()
+		else ()*)
