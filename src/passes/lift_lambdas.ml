743ec3368ff8803c5d2ab6d9d6e7accc009fad6d
Hans Montero <hjm2133@columbia.edu>
2019-05-13 14:58:14 -0400

    Support Just type in lambda lifting

--- src/passes/lift_lambdas.ml
+++ src/passes/lift_lambdas.ml
@@ -35,6 +35,7 @@ let rec transform_main d_list = match d_list with
 
 let rec m_replace og_ex m_ex ex = match ex with
 	| App(e1, e2) -> App(m_replace og_ex m_ex e1, m_replace og_ex m_ex e2)
+	| Just(e1) -> Just(m_replace og_ex m_ex e1)
 	| Tuple(e1, e2) -> Tuple(m_replace og_ex m_ex e1, m_replace og_ex m_ex e2)
 	| Ite(e1, e2, e3) -> Ite(m_replace og_ex m_ex e1, m_replace og_ex m_ex e2, m_replace og_ex m_ex e3)
 	| Lambda(e1, e2) -> Lambda(e1, m_replace og_ex m_ex e2)
@@ -77,6 +78,7 @@ let rec check_clauses clauses seen_v seen_f vnames wrapped_clauses= match clause
 let rec transform_comps expr = match expr with
 	| Lambda(e1, e2) -> Lambda(e1, transform_comps e2)
 	| App(e1, e2) -> App(transform_comps e1, transform_comps e2)
+	| Just(e1) -> Just(transform_comps e1)
 	| Ite(e1, e2, e3) -> Ite(transform_comps e1, transform_comps e2, transform_comps e3)
 	| Let(Assign(n, e1), e2) -> Let(Assign(n, transform_comps e1), transform_comps e2)
 	| InfList(e1) -> InfList(transform_comps e1)
@@ -142,6 +144,8 @@ let rec find_lambdas nested = function
 
     	(StringSet.empty, App(st2, st3))
 
+    | Just(e1) -> let (_, st1) = find_lambdas true e1 in
+    	(StringSet.empty, Just(st1))
 
     | Ite(e4, e5, e6) -> 
     	let (_, st4) = find_lambdas nested e4 in 
@@ -249,6 +253,10 @@ and get_closure_vars exp scope nested last_lam = match exp with
 		let (sc2, st2) = (get_closure_vars e5 scope nested false) in
 
 		(StringSet.union sc1 sc2, App(st1, st2))
+
+	| Just(e1) ->
+		let (sc1, st1) = (get_closure_vars e1 scope nested false) in
+		(sc1, Just(st1))
 
 	| Tuple(e1, e2) ->
 		let (sc1, st1) = (get_closure_vars e1 scope nested false) in
@@ -371,6 +379,7 @@ let rec repl_body body cl_to_mang tl_seen = match body with
 			| Some(mp) -> Var(mp) (* ok epic, it's a closure variable.. let's use the mangled parameter name *)
 			| _ -> Var(s1))) (* not from closure, just use the same name*)
 	| App(e1, e2) -> App(repl_body e1 cl_to_mang tl_seen, repl_body e2 cl_to_mang tl_seen)
+	| Just(e1) -> Just(repl_body e1 cl_to_mang tl_seen)
 	| Tuple(e1, e2) -> Tuple(repl_body e1 cl_to_mang tl_seen, repl_body e2 cl_to_mang tl_seen)
 	| Let(Assign(n, e1), e2) -> (match e1 with 
 		| Lambda(_, _) -> Let(Assign(n, e1), (if not tl_seen then (repl_body e2 cl_to_mang tl_seen) else e2))
@@ -394,6 +403,7 @@ and repl_bodyc body cl_to_mang tl_seen = match body with
 let rec mangle_close e nested tl_seen = match e with
 	| Var(s1) -> Var(s1)
 	| App(e1, e2) -> App(mangle_close e1 nested tl_seen, mangle_close e2 nested tl_seen)
+	| Just(e1) -> Just(mangle_close e1 nested tl_seen)
 	| Tuple(e1, e2) -> Tuple(mangle_close e1 nested tl_seen, mangle_close e2 nested tl_seen)
 	| Ite(e3, e4, e5) -> Ite(mangle_close e3 nested tl_seen, mangle_close e4 nested tl_seen, mangle_close e5 nested tl_seen)
 	| Let(Assign(n, rexp), inexp) -> (match rexp with
@@ -446,6 +456,9 @@ let rec lift exp decl_list = match exp with
 		let (body1, dlist1) = lift e1 decl_list in
 		let (body2, dlist2) = lift e2 dlist1 in
 		(App(body1, body2), dlist2)
+	| Just(e1) ->
+		let (body1, dlist1) = lift e1 decl_list in
+		(Just(body1), dlist1)
 	| Tuple(e1, e2) -> 
 		let (body1, dlist1) = lift e1 decl_list in
 		let (body2, dlist2) = lift e2 dlist1 in
