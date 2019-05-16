9b963608c79596eeea6cb174f6cd1bf2da79a809
Amanda Liu <al3623@columbia.edu>
2019-05-15 22:25:44 -0400

    Add color highlighting for test suite

--- tests/test_programs/run_tests.sh
+++ tests/test_programs/run_tests.sh
@@ -3,19 +3,19 @@
 
 for file in *.rpl
 do
-	rippl $file
+	rippl $file &> /dev/null
 	if [ $? -ne 0 ]  
-	then echo "failed test$var"
-   	else  echo "passed test$var"
+	then echo -e "\e[39m$file \e[91mfailed"
+   	else  echo -e "\e[39m$file \e[92mpassed"
 	fi
 
-	./${file::-4}
+	./${file::-4} &> /dev/null
 	if [ $? -ne 0 ]  
-	then echo "failed test$var"
-   	else  echo "passed test$var"
+	then echo -e "\e[39m$file \e[91mfailed"
+   	else echo -e "\e[39m$file \e[92mpassed"
 	fi
 	
-	rm ./${file::-4}
+	rm ./${file::-4} -rf &> /dev/null
 
 done
 
