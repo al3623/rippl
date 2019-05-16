eff9a17defaae3120f1eb011477c45e3d33242a6
Amanda Liu <al3623@columbia.edu>
2019-05-15 21:56:05 -0400

    Restructure inference testing directory

--- tests/inference/run_tests.sh
+++ tests/inference/run_tests.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 #assert.sh
 
-make
-cd inference_tests
-
 num_tests=$(ls -1q test* | wc -l)
 
 for var in `seq 1 1 $num_tests`
 do
-	../driver.native test$var > "$var.out"
-	file_diff=$(diff $var.out ../lexing_output)
-	rm "$var.out"
-	if [ -z "$file_diff"] 
-	then echo "passed $var"
-   	else  exit 1 
+	rippl -t test$var.rpl &> /dev/null
+	if [ $? -ne 0 ]  
+	then echo "failed test$var"
+   	else  echo "passed test$var"
 	fi
 done
 
-cd -
